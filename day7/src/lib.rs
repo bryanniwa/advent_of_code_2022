58288303280090d0b4d846937fc7abe69357c8f5
Bryan Niwa <bniwa@sfu.ca>
2022-12-25 15:47:22 -0800

    Tried fixing tree. Come back later

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cell::RefCell;
+
 pub fn part_one(lines: &[String]) {
     let mut file_system = Dir::default();
     let mut current = &file_system;
@@ -5,12 +7,13 @@ pub fn part_one(lines: &[String]) {
     for line in lines {
         if line.starts_with("$ cd") {
             let dir = line.split_whitespace().nth(2).unwrap();
-            if let Some(mut node) = current.subdirs.iter().find(|x| x.name == dir) {
+            let subdirs = current.borrow().subdirs;
+            if let Some(node) = subdirs.iter().find(|x| x.borrow().name == dir) {
                 current = node;
             } else {
                 let mut new_dir = Dir::new(dir.to_string(), &current);
-                current.as_mut().subdirs.push(new_dir);
-                current = current.subdirs.last_mut().unwrap();
+                current.borrow_mut().subdirs.push(new_dir);
+                current = current.borrow().subdirs.last_mut().unwrap();
             }
         }
     }
@@ -23,15 +26,15 @@ pub fn part_two(lines: &[String]) {
 #[derive(Debug)]
 struct Dir<'a> {
     name: String,
-    parent: Option<&'a Box<Dir<'a>>>,
+    parent: Option<&'a RefCell<Dir<'a>>>,
     size: usize,
     files: Vec<usize>,
-    subdirs: Vec<Box<Dir<'a>>>,
+    subdirs: Vec<RefCell<Dir<'a>>>,
 }
 
 impl Dir<'_> {
-    fn default<'a>() -> Box<Dir<'a>> {
-        Box::new(Dir {
+    fn default<'a>() -> RefCell<Dir<'a>> {
+        RefCell::new(Dir {
             name: String::from("/"),
             parent: None,
             size: 0,
@@ -40,8 +43,8 @@ impl Dir<'_> {
         })
     }
 
-    fn new<'a>(name: String, parent: &'a Box<Dir>) -> Box<Dir<'a>> {
-        Box::new(Dir {
+    fn new<'a>(name: String, parent: &'a RefCell<Dir>) -> RefCell<Dir<'a>> {
+        RefCell::new(Dir {
             name,
             parent: Some(parent),
             size: 0,
